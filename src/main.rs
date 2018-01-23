9e89f22aa3ea174c92c4fac7157e592b29ef9508
Slowyn <y231966mdk@gmail.com>
2018-01-23 17:05:31 +0700

    establish connection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
+#[macro_use]
 
 extern crate rocket;
 extern crate dotenv;
+extern crate diesel;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+fn establish_connection() -> PgConnection {
+    let db_url = dotenv::var("DATABASE_URL").expect("DB is not found. Shutdown");
+    PgConnection::establish(&db_url)
+        .expect(&format!("Error  connecting to {}", db_url))
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -10,6 +21,6 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    let db_url = dotenv::var("DATABASE_URL").expect("DB is not found. Shutdown");
+    let conn = establish_connection();
     rocket::ignite().mount("/", routes![index]).launch();
 }
